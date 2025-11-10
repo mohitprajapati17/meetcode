14b86245e8d0e8e296bf0afe6a3ad24a723907f7
mohitprajapati17 <mohitprajapati2444@gmail.com>
2025-11-11 01:47:04 +0530

    add  clerk

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import { env } from "./libs/env.js";
 import path from "path";
 const app=express();
+import connectDB from "./libs/db.js";
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -22,7 +23,13 @@ if(env.NODE_ENV==="production"){
 }
 
 
+const startServer=async()=>{
+    await  connectDB();
+    app.listen(env.PORT,()=>{
+        console.log(`Server is running on port ${env.PORT}`);
+    })
+}
 
-app.listen(env.PORT,()=>{
-    console.log(`Server is running on port ${env.PORT}`);
-});
+
+
+startServer();
